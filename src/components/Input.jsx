f094b10a28a44798cadf99f40731ae6798028ec3
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2024-06-29 19:22:59 +0300

    first commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+  return (
+    <div>
+      <label htmlFor='name'>Name:</label>
+      <input type='text' id='name' placeholder='Name..' />
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import Form from '../../components/Form';
+
+export function LoginForm() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <Form />
+    </div>
+  );
+}
